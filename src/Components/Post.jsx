5d5e1e46a0bc572703ae075ea634ded7d2add1e0
Bigleoo <leonardo.v.m.ramoss@gmail.com>
2023-04-14 12:50:55 -0300

    Assistir Aula usestate

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -32,7 +32,7 @@ export function Post({author, content, publishedAt}){
                         <span>{author.role}</span>
                     </div>
                 </div>
-
+                
                 <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
 
             </header>
